54ad61a8412d6e1130aec82ccec37f6e808e1415
gka0903 <ham3252@naver.com>
2023-01-29 08:36:06 +0900

    change

---
+++ 2023/2023.01/2023.01.28/Valid Parentheses.py
@@ -0,0 +1,22 @@
+def isValid(s):
+    arr = []
+    arr = []
+    for i in s:
+        if i == '(' or i == '[' or i == '{':
+            arr.append(i)
+        elif i == ')' and arr and arr[-1] == '(':
+            arr.pop()
+        elif i == ']' and arr and arr[-1] == '[':
+            arr.pop()
+        elif i == '}' and arr and arr[-1] == '{':
+            arr.pop()
+        else:
+            return False
+
+    if not arr:
+        return True
+    else:
+        return False
+
+
+print(isValid("("))
