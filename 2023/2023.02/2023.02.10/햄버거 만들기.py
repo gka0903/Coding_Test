c982c12862d73462b62ae82b707ddfaa0c10fa13
gka0903 <ham3252@naver.com>
2023-02-10 11:47:43 +0900

    change

---
+++ 2023/2023.02/2023.02.10/햄버거 만들기.py
@@ -0,0 +1,16 @@
+# 1 : 빵
+# 2 : 야채
+# 3 : 고기
+def solution(ingredient):
+    check_arr = [1, 2, 3, 1]
+    answer = 0
+    arr = []
+    for i in ingredient:
+        arr.append(i)
+        if len(arr) >= 4 and arr[len(arr) - 4::] == check_arr:
+            del arr[len(arr) - 4::]
+            answer += 1
+    return answer
+
+
+print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]))
