38a04e0e8337e1a433a3f1724f7a4c3cc0a4d4c4
gka0903 <ham3252@naver.com>
2023-02-12 21:57:44 +0900

    change

---
+++ 2023/2023.02/2023.02.12/숫자 짝꿍.py
@@ -0,0 +1,19 @@
+def solution(X, Y):
+    answer = ''
+    arr_x = [0] * 10
+    arr_y = [0] * 10
+    for i in X:
+        arr_x[int(i)] += 1
+    for i in Y:
+        arr_y[int(i)] += 1
+    for num in range(9, -1, -1):
+        m = min(arr_x[num], arr_y[num])
+        answer += str(num) * m
+    if len(answer) == 0:
+        return '-1'
+    if answer[0] == '0':
+        return '0'
+    return answer
+
+
+print(solution("100", "123450"))
