56d0e41f0083cc02a9d4700c8f650d0477d0d780
gka0903 <ham3252@naver.com>
2023-02-15 17:18:13 +0900

    change

---
+++ 2023/2023.02/2023.12.15/How Many Numbers Are Smaller Than the Current Number.py
@@ -0,0 +1,11 @@
+def smallerNumbersThanCurrent(nums):
+    nums_len = len(nums)
+    answer = [0] * nums_len
+    for i in range(nums_len):
+        for j in range(nums_len):
+            if nums[i] > nums[j]:
+                answer[i] += 1
+    return answer
+
+
+print(smallerNumbersThanCurrent([8, 1, 2, 2, 3]))
