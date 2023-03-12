ed0208a7d8112bb5c99fd404570caa65f43185f2
gka0903 <ham3252@naver.com>
2023-03-13 00:59:12 +0900

    change

---
+++ 2023/2023.03/2023.03.12/Maximum Length of Pair Chain.py
@@ -0,0 +1,13 @@
+def findLongestChain(pairs):
+    pairs.sort()
+    print(pairs)
+    dp = [1] * len(pairs)
+    for i in range(1, len(pairs)):
+        for j in range(i):
+            if pairs[i][0] > pairs[j][1]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    return max(dp)
+
+
+print(findLongestChain([[7, 9], [4, 5], [7, 9], [-7, -1], [0, 10], [3, 10], [3, 6], [2, 3]]))
