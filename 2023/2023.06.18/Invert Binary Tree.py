2f49e723bb724d21ec0621b52a4e1dd5d04fde5c
gka0903 <ham3252@naver.com>
2023-06-19 18:22:39 +0900

    change

---
+++ 2023/2023.06.18/Invert Binary Tree.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, value=None, lvalue=None, rvalue=None):
+        self.value = value
+        self.left = lvalue
+        self.right = rvalue
+
+
+class Tree:
+    def __init__(self, root=None):
+        self.root = root
+        None
