a60fb4f7c7389e48e3ae555f4d1d68c587c6fd21
gka0903 <ham3252@naver.com>
2023-10-25 09:57:45 +0900

    change

--- 문제/2023/10/연속된 부분 수열의 합.py
+++ 문제/2023/10/연속된 부분 수열의 합.py
@@ -1,18 +1,31 @@
 def solution(sequence, k):
     start, end = 0, 0
-
-    if k in sequence:
-        index = sequence.index(k)
-        return [index, index]
-
-    while end < len(sequence) - 1:
-        s = sum(sequence[start:end + 1])
-        if s == k:
-            return [start, end]
-        elif s < k:
-            end += 1
-        else:
+    current_sum = sequence[0]
+    results = []
+    while start < len(sequence) and end < len(sequence):
+        if current_sum == k:
+            results.append([start, end])
+            current_sum -= sequence[start]
             start += 1
+        elif current_sum < k:
+            end += 1
+            if end < len(sequence):
+                current_sum += sequence[end]
+        else:
+            current_sum -= sequence[start]
+            start += 1
+    if not results:
+        return [0, 0]
+
+    result = [0, 0]
+    check = float('inf')
+    for i in results:
+        index_distance = i[1] - i[0]
+        if index_distance < check:
+            check = index_distance
+            result = i
+    return result
 
 
 print(solution([2, 2, 2, 2, 2], 6))
+print(solution([1, 1, 1, 2, 3, 4, 5], 5))
