90c7d04e86c18aeb6f957cd494c63b61fbd35cac
gka0903 <ham3252@naver.com>
2023-12-08 10:42:15 +0900

    주석

--- 문제/twoPointer/Two Sum II - Input Array Is Sorted.py
+++ 문제/twoPointer/Two Sum II - Input Array Is Sorted.py
@@ -5,6 +5,8 @@ def solution(numbers, target):
     while point_one <= point_two:
         sum_number = numbers[point_one] + numbers[point_two]
         if sum_number == target:
+            # 문제에서는 특이하게 인덱스 값에 하나를 더한 값들을 리턴하게 한다
+            # 제출시에는 [point_one + 1, point_two + 1]로 제출하면 된다
             return [point_one, point_two]
         elif sum_number > target:
             point_two -= 1
