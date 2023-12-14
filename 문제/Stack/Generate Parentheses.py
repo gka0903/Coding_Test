b921fe49090fc2d355b6de6442d97c6b5ed9488c
gka0903 <ham3252@naver.com>
2023-12-14 15:40:40 +0900

    stack

---
+++ 문제/Stack/Generate Parentheses.py
@@ -0,0 +1,23 @@
+def solution(n):
+    stack = []
+    res = []
+
+    def backTrack(openN, closeN):
+        if openN == closeN == n:
+            res.append("".join(stack))
+            return
+        if openN < n:
+            stack.append("(")
+            backTrack(openN + 1, closeN)
+            stack.pop()
+
+        if closeN < openN:
+            stack.append(")")
+            backTrack(openN, closeN + 1)
+            stack.pop()
+
+    backTrack(0, 0)
+    return res
+
+
+print(solution(3))
---
+++ 문제/Stack/Min Stack.py
@@ -0,0 +1,30 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minStack:
+            m = min(self.minStack[-1], val)
+            self.minStack.append(m)
+        else:
+            self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.minStack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
