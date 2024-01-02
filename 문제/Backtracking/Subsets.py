7f274b270d2cf744c499f9ce26694b98be5094e0
gka0903 <ham3252@naver.com>
2024-01-02 22:24:02 +0900

    changes

--- 문제/Backtracking/Subsets.py
+++ 문제/Backtracking/Subsets.py
@@ -2,10 +2,18 @@ def solution(nums):
     res = []
     stack = []
 
-    def backTracking():
+    def backTracking(index):
+        if index == len(nums):
+            index -= 1
+            return
         res.append(stack)
+        stack.append(nums[index])
+        backTracking(index + 1)
+        stack.pop()
 
-    return 0
+    backTracking(0)
+
+    return res
 
 
 print(solution([1, 2, 3]))
