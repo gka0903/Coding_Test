4da8edddf22fe99ad10db1326e64b56a85d6572a
gka0903 <ham3252@naver.com>
2024-01-05 18:04:24 +0900

    백트래킹 subsets

--- 문제/Backtracking/Subsets.py
+++ 문제/Backtracking/Subsets.py
@@ -2,16 +2,19 @@ def solution(nums):
     res = []
     stack = []
 
-    def backTracking(index):
-        if index == len(nums):
-            index -= 1
+    def dfs(index):
+        # 확인된 index가 기존 list 길이보다 길어지면 그 값을 받고 함수를 종료
+        if index >= len(nums):
+            res.append(stack.copy())
             return
-        res.append(stack)
+
+        # 자기자신이 포함된 경우와 포함되지 않은 경우를 모두 체크
+        dfs(index + 1)
         stack.append(nums[index])
-        backTracking(index + 1)
+        dfs(index + 1)
         stack.pop()
 
-    backTracking(0)
+    dfs(0)
 
     return res
 
