9e860edb9f74ee09b1a4108e63bd832fa3a9006a
gka0903 <ham3252@naver.com>
2024-02-01 23:11:40 +0900

    기타 - 효도

--- 문제/기타/효도.py
+++ 문제/기타/효도.py
@@ -1,22 +1,20 @@
 def solution(matrix):
     result = float('inf')
     for i in range(3):
-        check1 = 0
-        check2 = 0
-        s1 = sum([matrix[i][0], matrix[i][1], matrix[i][2]])
-        s2 = sum([matrix[0][i], matrix[1][i], matrix[2][i]])
-        avg1 = s1 // 3
-        avg2 = s2 // 3
+        # 1 2 3
+        for j in range(1, 4):
+            check = 0
+            for num in matrix[i]:
+                check += abs(num - j)
+            result = min(result, check)
 
-        for num in matrix[i]:
-            check1 += abs(num - avg1)
-        result = min(check1, result)
-
-        for num in [matrix[0][i], matrix[1][i], matrix[2][i]]:
-            check2 += abs(num - avg2)
-        result = min(check2, result)
+        for j in range(1, 4):
+            check = 0
+            for num in [matrix[0][i], matrix[1][i], matrix[2][i]]:
+                check += abs(num - j)
+            result = min(result, check)
 
     return result
 
 
-print(solution([[1, , 3], [1, 1, 2], [1, 1, 3]]))
+print(solution([[1, 2, 3], [1, 1, 2], [2, 1, 3]]))
