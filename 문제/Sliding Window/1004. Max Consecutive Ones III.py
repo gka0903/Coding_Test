e6130e342b2bd900c87aff7f4f3504dca51ac6f0
gka0903 <ham3252@naver.com>
2024-07-02 19:21:16 +0900

    sliding window -

---
+++ 문제/Sliding Window/1004. Max Consecutive Ones III.py
@@ -0,0 +1,1 @@
+# https://leetcode.com/problems/max-consecutive-ones-iii/description/?envType=study-plan-v2&envId=leetcode-75
---
+++ 문제/Sliding Window/1456. Maximum Number of Vowels in a Substring of Given Length.py
@@ -0,0 +1,20 @@
+def solution(s, k):
+    maximum_number = 0
+    current_number = 0
+    vowel = 'aeiou'
+
+    for i in range(len(s)):
+        if s[i] in vowel:
+            current_number += 1
+        if i >= k and s[i - k] in vowel:
+            current_number -= 1
+
+        if current_number > maximum_number:
+            maximum_number = current_number
+
+        if maximum_number == k
+
+    return maximum_number
+
+
+print(solution("abciiidef", 3))
