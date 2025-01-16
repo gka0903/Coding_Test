fce7ab93aeac3f0f22c3f4b0cf6f3c424c331cf6
gka0903 <ham3252@naver.com>
2025-01-16 16:21:55 +0900

    25/01/16

--- 문제/명예의전당.py
+++ 문제/명예의전당.py
@@ -1,3 +1,24 @@
+def binary(fame, score):
+    first, end = 0, len(fame)
+
+    while first < end:
+        mid = (first + end) // 2
+        if fame[mid] <= score:
+            first = mid + 1
+        else:
+            end = mid
+
+    return fame[:first] + [score] + fame[first:]
+
+
 def solution(k, score):
+    fame = []
     answer = []
+
+    for s in score:
+        fame = binary(fame, s)  # 이진 탐색으로 점수 삽입
+        if len(fame) > k:
+            fame.pop(0)  # 명예의 전당 크기가 k를 초과하면 최솟값 제거
+        answer.append(fame[0])  # 최하위 점수 추가
+
     return answer
