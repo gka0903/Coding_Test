309b0622c8235a8e864697cd2308f51ccc77c6a6
gka0903 <hham5879@gmail.com>
2025-12-10 17:23:25 +0900

    백준 내리막길

--- 스터디/내리막 길.py
+++ 스터디/내리막 길.py
@@ -1,9 +1,10 @@
 import sys
 
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 M, N = map(int, input().split())
 m = []
-dp = [[0 for _ in range(N)] for _ in range(M)]
+dp = [[-1 for _ in range(N)] for _ in range(M)]
 d = (
     (-1, 0),
     (1, 0),
@@ -12,24 +13,29 @@ d = (
 )
 
 
-def dfs(y, x, cnt):
-    dp[y][x] += cnt
-
+def dfs(y, x):
     if y == M - 1 and x == N - 1:
-        return
+        return 1
+
+    if dp[y][x] != -1:
+        return dp[y][x]
+
+    dp[y][x] = 0
 
     for dy, dx in d:
         ny, nx = dy + y, dx + x
 
         if 0 <= ny < M and 0 <= nx < N:
             if m[ny][nx] < m[y][x]:
-                dfs(ny, nx, dp[y][x])
+                dp[y][x] += dfs(ny, nx)
+
+    return dp[y][x]
 
 
 for _ in range(M):
     li = list(map(int, input().split()))
     m.append(li)
 
-dfs(0, 0, 1)
+dfs(0, 0)
 
-print(dp)
+print(dp[0][0])
