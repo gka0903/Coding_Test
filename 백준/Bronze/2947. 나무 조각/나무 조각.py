c881aebb9cc34469d3df1945307f751580096fdf
Haam HyoungBum <hham5879@gmail.com>
2025-12-23 14:35:33 +0900

    [Bronze I] Title: 나무 조각, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+# 첫 번째 조각의 수가 두 번째 수보다 크다면, 둘의 위치를 서로 바꾼다.
+# 두 번째 조각의 수가 세 번째 수보다 크다면, 둘의 위치를 서로 바꾼다.
+# 세 번째 조각의 수가 네 번째 수보다 크다면, 둘의 위치를 서로 바꾼다.
+# 네 번째 조각의 수가 다섯 번째 수보다 크다면, 둘의 위치를 서로 바꾼다.
+# 만약 순서가 1, 2, 3, 4, 5 순서가 아니라면 1 단계로 다시 간다.
+# 처음 조각의 순서가 주어졌을 때, 위치를 바꿀 때 마다 조각의 순서를 출력하는 프로그램을 작성하시오.
+
+trees = list(map(int, input().split()))
+
+while trees != [1, 2, 3, 4, 5]:
+    for i in range(1, 5):
+        if trees[i - 1] > trees[i]:
+            trees[i - 1], trees[i] = trees[i], trees[i - 1]
+            print(*trees)
