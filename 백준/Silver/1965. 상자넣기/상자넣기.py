91382171f6b3dcf4103811664b3a00b6f9a9a568
Haam HyoungBum <hham5879@gmail.com>
2026-01-23 09:45:15 +0900

    [Silver II] Title: 상자넣기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.py
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -3,11 +3,31 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 boxes = list(map(int, input().split()))
-dp = [1] * N
+longest_subsequence = [boxes[0]]
+result = 1
 
-for i in range(1, N):
-    for j in range(i):
-        if boxes[i] > boxes[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
 
-print(max(dp))
+def binary_search(s, e, target):
+    idx = e
+
+    while s <= e:
+        m = (s + e) // 2
+
+        if longest_subsequence[m] >= target:
+            idx = m
+            e = m - 1
+        else:
+            s = m + 1
+
+    return idx
+
+
+for i in range(1, len(boxes)):
+    if boxes[i] > longest_subsequence[-1]:
+        longest_subsequence.append(boxes[i])
+        result += 1
+    else:
+        box_idx = binary_search(0, result - 1, boxes[i])
+        longest_subsequence[box_idx] = boxes[i]
+
+print(result)
